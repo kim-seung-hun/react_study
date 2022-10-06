31423a6c42f222bf83041f3876c2f4559ac53611
kim-seung-hun <rlatmdgns16@naver.com>
2022-10-06 17:37:12 +0900

    강의 3일차

---
+++ 개인공부/5.movie/src/routes/Home.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import Movie from "./components/Movie";
+
+function Home() {
+  const [loading, setLoaing] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const res = await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+    );
+    const json = await res.json();
+    setMovies(json.data.movies);
+    setLoaing(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  // useEffect(() => {
+  //   fetch(
+  //     "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+  //   )
+  //     .then((res) => res.json())
+  //     .then((json) => {
+  //       setMovies(json.data.movies);
+  //       setLoaing(false);
+  //     });
+  // }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Home;
